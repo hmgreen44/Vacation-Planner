ca776d6dee52b365b1bcba1c37e33847a1de9991
hmgreen44@gmail.com <68399145+hmgreen44@users.noreply.github.com>
2021-05-04 16:55:28 -0400

    added forms to create trip page

--- src/pages/CreateTripPage.js
+++ src/pages/CreateTripPage.js
@@ -3,21 +3,28 @@ import React from 'react'
 export default function CreateTripPage() {
     return (
         <div>
-            <div className="createTrip container fluid text-center">
-                <div className="row position-relative justify-content-center text-center mt-5 pt-4">
-                    <div className="col-md-12 text-center">
-                        <div className="heroText text-center mb-4 mt-5">
+            <div className="createTrip container fluid">
+                <div className="row position-relative justify-content-center mt-5 pt-4">
+                    <div className="col-md-12">
+                        <div className="heroText mb-4 mt-5">
                             <div className="trans card p-5">
-                                <div className="heroText">
-                                    <h1>Create a New Trip</h1>  
-                                    <p>Please fill out the fields provided below</p>                                 
-                                        </div>
-                                    </div>
+                                <div className="heroText text-center">
+                                    <h1>Create a New Trip</h1>
+                                    <p>Please fill out the fields provided below</p>
+                                </div>
+                                <div className="col-md-6">
+                                    <form>
+                                        <label htmlFor="Destination" className="form-label">Destination</label>
+                                        <input type="text"
+                                        className="form-control" />
+                                    </form>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
     )
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,17 +7,17 @@ import { useAuth } from '../utilities/AuthContext'
 export default function HomePage() {
     const { logout } = useAuth()
 
-    
+
     return (
         <div>
             <div className="homePage container fluid text-center">
-                <div className="row text-center pt-4">
+                <div className="row text-center mt-4 pt-4">
+                    <h1>Welcome Back User</h1>
                     <div className="col-md-12 text-center">
+                        <button onClick={logout} className="btn btn-dark mx-4 mt-3 position-absolute top-0 end-0">Logout</button>
                         <div className="heroText text-center mb-4 mt-5">
                             <div className="trans card p-5">
                                 <div className="heroText">
-                                <button onClick={logout} className="btn btn-dark mx-4 mt-3 position-absolute top-0 end-0">Logout</button>
-                                    <h1>HomePage</h1>
                                     <p>View Your Current Trips</p>
                                     <p>Oops! Looks like you don't have any trips planned.</p>
                                     <Link to="/create/">
